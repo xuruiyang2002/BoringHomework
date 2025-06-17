2cb450ee91b85a40ca3aad32ace133b6258de365
Ruiyang Xu <xury5520@outlook.com>
2025-06-17 19:40:00 +0800

    Update

--- Testing Demo/Testing of Isolation/memory_isolation.c
+++ Testing Demo/Testing of Isolation/memory_isolation.c
@@ -5,7 +5,13 @@
 #include <time.h>
 #include <stdint.h>
 
-#define BUFFER_SIZE (100 * 1024 * 1024) // 100MB buffer
+#define BUFFER_SIZE (10 * 1024 * 1024) // 10MB buffer
+
+/*
+ Allocates 10MB buffer to stress memory subsystem
+ Measures full write operation time
+ Tests if memory pressure from other VMs affects access latency
+ */
 
 void test_memory_isolation() {
     uint8_t *buffer = malloc(BUFFER_SIZE);
@@ -16,15 +22,15 @@ void test_memory_isolation() {
 
     struct timespec start, end;
     clock_gettime(CLOCK_REALTIME, &start);
-    
+
     // Write to entire buffer
     for (size_t i = 0; i < BUFFER_SIZE; i++) {
         buffer[i] = i & 0xFF;
     }
-    
+
     clock_gettime(CLOCK_REALTIME, &end);
     long duration = (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
     printf("Memory write time: %ld ns\n", duration);
-    
+
     free(buffer);
 }
